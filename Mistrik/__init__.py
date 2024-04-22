d2199d940fe2b19a17804ced7f5ef3eb40614349
MatusValko <mvmatusvalko@gmail.com>
2024-04-22 18:15:11 +0200

    Add algorithm

---
+++ Mistrik/__init__.py
@@ -0,0 +1,9 @@
+"""
+mistrik
+
+Score the readability of text using the Mistrik's measure of comprehensibility/readability Python library.
+"""
+
+__version__ = "1.0.0"
+__author__ = 'Matúš Valko'
+__credits__ = 'UKF FPVaI'
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='mistrik',
+    version='1.0.0',    
+    description='Measure text comprehensibility/readability',
+    url='https://github.com/MatusValko/Mistrik-measure-of-comprehensibility',
+    author='Matúš Valko',
+    author_email='mvmatusvalko@gmail.com',    
+    license='MIT',
+    packages=['mistrik'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License', 
+        'Natural Language :: Slovak',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',   
+        'Topic :: Scientific/Engineering',     
+        'Topic :: Text Processing :: Linguistic',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.12',
+    ],
+)  
