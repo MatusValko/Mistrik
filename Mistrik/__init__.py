958fc7d5afb98b7f4d3ca2e646c2f57383a0a1b5
Matúš Valko <72170259+MatusValko@users.noreply.github.com>
2024-04-22 21:27:01 +0200

    Update __init__.py

--- Mistrik/__init__.py
+++ Mistrik/__init__.py
@@ -4,6 +4,6 @@ mistrik
 Score the readability of text using the Mistrik's measure of comprehensibility/readability Python library.
 """
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__ = 'Matúš Valko'
 __credits__ = 'UKF FPVaI'
