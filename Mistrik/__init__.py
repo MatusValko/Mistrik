9b1317acca8c7fcbc77eac79fcbf11404602d744
MatusValko <mvmatusvalko@gmail.com>
2024-04-24 23:09:52 +0200

    change readme on pypi

--- Mistrik/__init__.py
+++ Mistrik/__init__.py
@@ -3,7 +3,7 @@
 Score the readability of text using the Mistrik's measure of comprehensibility/readability Python library.
 """
 __title__ = 'mistrik measure of readability'
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = 'Matúš Valko'
 __license__ = 'MIT'
 
